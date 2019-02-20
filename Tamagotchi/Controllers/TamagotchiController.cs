e475a4a3751ffb64146e7cd4d556314ec2c961ee
Kenny Halim and Pavel Zanchuk <pavel@>
2019-02-20 14:15:16 -0800

    ready project

---
+++ Tamagotchi/Controllers/TamagotchiController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using Tamagotchi.Models;
+using System.Collections.Generic;
+
+namespace Tamagotchi.Controllers
+{
+  public class TamagotchiController : Controller
+  {
+
+    [HttpGet("/tamagotchi")]
+    public ActionResult Index()
+    {
+      List<Pet> allPets = Pet.GetAll();
+      return View(allPets);
+    }
+
+    [HttpGet("/tamagotchi/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/tamagotchi")]
+    public ActionResult Create(string name)
+    {
+        Pet myPet = new Pet(name, 70, 70, 70);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet("/tamagotchi/{id}")]
+    public ActionResult Show(int id)
+    {
+      Pet pet = Pet.Find(id);
+      return View(pet);
+    }
+
+    [HttpPost("/tamagotchi/{id}/addfood")]
+    public ActionResult Update(int id)
+    {
+      Pet pet = Pet.Find(id);
+      pet.AddFood();
+      return RedirectToAction("Show");
+    }
+
+    [HttpPost("/tamagotchi/{id}/addrest")]
+    public ActionResult Update2(int id)
+    {
+      Pet pet = Pet.Find(id);
+      pet.AddRest();
+      return RedirectToAction("Show");
+    }
+
+    [HttpPost("/tamagotchi/{id}/addattention")]
+    public ActionResult Update3(int id)
+    {
+      Pet pet = Pet.Find(id);
+      pet.AddAttention();
+      return RedirectToAction("Show");
+    }
+
+    [HttpPost("/tamagotchi/{id}/timepass")]
+    public ActionResult Update4(int id)
+    {
+      Pet pet = Pet.Find(id);
+      pet.TimePass();
+      return RedirectToAction("Show");
+    }
+
+    [HttpPost("/tamagotchi/{id}/delete")]
+    public ActionResult Update5(int id)
+    {
+      Pet pet = Pet.Find(id);
+      pet.Delete(id);
+      return RedirectToAction("Index");
+    }
+
+    }
+  }
---
+++ Tamagotchi/Models/Pet.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+
+namespace Tamagotchi.Models
+{
+  public class Pet
+  {
+    private string _name;
+    private int _food;
+    private int _attention;
+    private int _rest;
+    private static List<Pet> _petList  = new List<Pet> {};
+    private int _id;
+
+    public Pet (string name, int food, int attention, int rest)
+    {
+      _name = name;
+      _food = food;
+      _attention = attention;
+      _rest = rest;
+      _petList.Add(this);
+      _id = _petList.Count;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public int GetFood()
+    {
+      return _food;
+    }
+
+    public int AddFood()
+    {
+      if(_food >= 100){
+        _food = 100;
+      } else {
+        _food = _food + 5;
+      }
+      return _food;
+    }
+
+    public int GetAttention()
+    {
+      return _attention;
+    }
+
+    public int AddAttention()
+    {
+      if(_attention >= 100){
+        _attention = 100;
+      } else {
+        _attention = _attention + 5;
+      }
+      return _attention;
+    }
+
+    public int GetRest()
+    {
+      return _rest;
+    }
+
+    public int AddRest()
+    {
+      if(_rest >= 100){
+        _rest = 100;
+      } else {
+        _rest = _rest + 5;
+      }
+      return _rest;
+    }
+
+    public void TimePass(){
+      if(_food <= 0 || _rest <= 0 || _attention <= 0){
+        _food = 0;
+        _rest = 0;
+        _attention = 0;
+      } else {
+      _food = _food - 10;
+      _rest = _rest - 10;
+      _attention = _attention - 10;
+    }
+    }
+
+    public void Delete(int id){
+      _petList.RemoveAt(id-1);
+    }
+
+    public static List<Pet> GetAll()
+    {
+      return _petList;
+    }
+
+    public static void ClearAll()
+    {
+      _petList.Clear();
+    }
+
+    public static Pet Find(int searchId)
+    {
+      return _petList[searchId-1];
+    }
+
+    public int GetId()
+   {
+     return _id;
+   }
+  }
+}
